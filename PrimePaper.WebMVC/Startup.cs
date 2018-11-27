5870ffb1b2c5cd96effc3dc92b639271e9c0140b
Edycarlos018 <edycarlos@gmail.com>
2018-11-27 15:04:17 -0500

    Add project files.

---
+++ PrimePaper.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrimePaper.WebMVC.Startup))]
+namespace PrimePaper.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
